ffe22f70ac37c87cea6d37e7d16ee03e915de0bd
Joan Rieu <joan@joanrieu.com>
2018-08-11 02:36:39 +0200

    compute state and render tasks

--- app.tsx
+++ app.tsx
@@ -7,7 +7,17 @@ type TodoEvent =
   | { type: "task_checked", id: ID }
   | { type: "task_deleted", id: ID }
 
-import { observable } from "mobx"
+interface TodoState {
+  tasks: Map<ID, Task>
+}
+
+interface Task {
+  id: ID,
+  name: TaskName,
+  checked: boolean
+}
+
+import { observable, computed } from "mobx"
 
 class TodoApp {
   @observable events: TodoEvent[] = []
@@ -27,6 +37,33 @@ class TodoApp {
   deleteTask(id: ID) {
     this.events.push({ type: "task_deleted", id })
   }
+
+  @computed get state() {
+    const state: TodoState = {
+      tasks: new Map()
+    }
+    for (const event of this.events) {
+      switch (event.type) {
+        case "task_created":
+          state.tasks.set(event.id, {
+            id: event.id,
+            name: "",
+            checked: false
+          })
+          break
+        case "task_renamed":
+          state.tasks.get(event.id)!.name = event.name
+          break
+        case "task_checked":
+          state.tasks.get(event.id)!.checked = true
+          break
+        case "task_deleted":
+          state.tasks.delete(event.id)
+          break
+      }
+    }
+    return state
+  }
 }
 
 import { h, render, Component } from "preact"
@@ -35,14 +72,32 @@ import { observer } from "mobx-preact"
 const app = window.app = new TodoApp()
 
 @observer
-class TodoView extends Component<{ app: TodoApp }> {
+class TodoAppView extends Component<{ app: TodoApp }> {
   render() {
+    const { app } = this.props
     return (
       <div>
-        {JSON.stringify(this.props.app.events)}
+      {
+        [...app.state.tasks.values()].map(task =>
+          <TaskView app={app} task={task} />
+        )
+      }
       </div>
     )
   }
 }
 
-render(<TodoView app={app} />, document.body)
+@observer
+class TaskView extends Component<{ app: TodoApp, task: Task }> {
+  render() {
+    const { app, task } = this.props
+    return (
+      <div>
+        {task.checked ? "☑ " : "☐ "}
+        {task.name}
+      </div>
+    )
+  }
+}
+
+render(<TodoAppView app={app} />, document.body)
