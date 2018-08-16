abc9e4d1422d6055f399c2ef54e1375764a569ba
Joan Rieu <joan@joanrieu.com>
2018-08-16 22:31:52 +0200

    introduce src & build folders

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,10 @@ import commonjs from "rollup-plugin-commonjs"
 import typescript from "rollup-plugin-typescript2"
 
 export default {
-  input: "app.tsx",
+  input: "src/app.tsx",
   output: {
     name: "TodoApp",
-    file: "bundle.js",
+    file: "build/bundle.js",
     format: "iife"
   },
   plugins: [
